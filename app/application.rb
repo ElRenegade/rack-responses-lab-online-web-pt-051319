cb97bee8cd1310e059dd5b51eb6cc82f5d7de068
Santiago Sanchez <chagostwo@yahoo.com>
2019-08-08 02:32:40 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,8 @@
 class Application
   def call(env)
     resp = Rack::Response.new
-    resp.write.Time.now
+    t = Time.now
+    resp.write "#{t}"
     # if Time.now.hour.between?(0,11)
     #   resp.write "Good Morning!"
     # elsif Time.now.hour.between?(12,24)
